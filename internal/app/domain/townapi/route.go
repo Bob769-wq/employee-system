21dc13579bf74aa0c8250d8ecc63a1c137b88617
GoodGoodRu <sammayainfo@gmail.com>
2025-07-15 14:31:11 +0800

    feat: prep town api

---
+++ internal/app/domain/townapi/route.go
@@ -0,0 +1,26 @@
+package townapi
+
+import (
+	"net/http"
+
+	"github.com/mayainfo/employee-practice-be/internal/business/domain/town"
+	"github.com/mayainfo/employee-practice-be/internal/business/sdk/tran"
+	"github.com/mayainfo/employee-practice-be/internal/framework/logger"
+	"github.com/mayainfo/employee-practice-be/internal/framework/web"
+)
+
+// Config contains all the mandatory dependencies for this group of handlers.
+type Config struct {
+	Log  *logger.Logger
+	TxM  tran.TxManager
+	Town *town.Core
+}
+
+func CustomerRoutes(app *web.App, cfg Config) {
+	const version = "v1"
+
+	hdl := newHandlers(cfg.Log, cfg.TxM, cfg.Town)
+
+	app.HandleFunc(http.MethodGet, version, "/cities", hdl.queryCities)
+	app.HandleFunc(http.MethodGet, version, "/cities/{cityID}/towns", hdl.queryByCityID)
+}

78d964892756955eb52dd1c9657571afd49188ef
Bob <dash78231@gmail.com>
2025-07-22 09:27:28 +0800

    employeelist set

--- web/apps/practice/src/app/employee-list.component.ts
+++ web/apps/practice/src/app/employee-list.component.ts
@@ -6,7 +6,31 @@ import { EmployeeQueryService } from './employee-query';
 @Component({
   selector: 'app-employee-list',
   imports: [],
-  template: ``,
+  template: `
+    <h1 class="text-2xl font-bold">Employee List</h1>
+    @if (employeeQuery.isPending()) {
+      Loading...
+    }
+    @if (employeeQuery.error()) {
+      ERROR!
+    }
+    @if (employeeQuery.data(); as data) {
+      <div class="space-y-4">
+        @for (employee of data.items; track employee.id) {
+          <div>
+            <div>
+              <span class="font-medium"
+                >{{ employee.lastName }} {{ employee.firstName }}
+              </span>
+              <span>{{ employee.cellphone }}</span>
+              <span>ID{{ employee.id }}</span>
+              <span>{{ employee.email }}</span>
+            </div>
+          </div>
+        }
+      </div>
+    }
+  `,
 })
 export class EmployeeListComponent {
   employeeQueryService = inject(EmployeeQueryService);
